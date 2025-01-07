fcbd5344019dadde318800c8e3741a980fd2ceb3
Daniel Cuznetov <danielcuznetov04@gmail.com>
2025-01-07 16:24:48 +0200

    Add support for serving static files in development.
    Updated `urlpatterns` to include a static files handler using `django.conf.urls.static`. This change ensures that static files are properly served during development when `DEBUG` mode is enabled.

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -26,4 +28,4 @@ urlpatterns = [
     path("", include("apps.ensurance.urls")),
     path("", include("apps.common.urls")),
     path("", include("apps.payment.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
